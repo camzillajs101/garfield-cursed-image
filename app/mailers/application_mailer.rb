6cfe803a7605555c969f8ef34c0e421fca3fd94a
Camilo Mason <camilomason@Camilos-MBP.home>
2019-05-12 21:23:39 -0400

    trying one more thing for mailer?
    whyyyy is this not workinnggggg???

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'info@garfieldcursedimage.org'
+  default from: 'garfieldcursedimage@gmail.com'
   layout 'mailer'
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   # }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: 'info@garfieldcursedimage.org'}
+  config.action_mailer.default_options = {from: 'garfieldcursedimage@gmail.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
