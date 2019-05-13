c8a5cd486397e6cc7870b3e6995d6c8a6a2cf8e3
Camilo Mason <camilomason@Camilos-MBP.home>
2019-05-13 07:09:26 -0400

    and updated routes
    and some poivrons

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
-  get 'requests/index'
-  get 'requests/show'
-  get 'requests/new'
-  get 'requests/create'
-  get 'requests/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'images#index'
   resources :images, only: [:index, :show, :new, :create] do
     resources :comments, shallow: true
   end
+  resources :requests, except: [:edit, :update]
 end
