02fe7389cab349bd870d1c776600bb988d8e12fd
Camilo Mason <camilomason@Camilos-MBP.home>
2019-05-12 21:38:19 -0400

    mailer not working
    WHY THE HELL ISNT"SE WIHOIHOI MAILER UAHSKJHSDKJAHFAUEIDJCCKCKFYUASOOUUUFOU AMIELR MAiler IDBVGJKDHaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- app/mailers/image_mailer.rb
+++
@@ -1,5 +0,0 @@
-class ImageMailer < ApplicationMailer
-  def welcome_email
-    mail(to: "chamomilemasone@gmail.com", subject: 'Welcome to My Awesome Site')
-  end
-end
--- test/mailers/image_mailer_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ImageMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
