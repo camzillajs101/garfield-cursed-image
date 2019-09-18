fae53798be8b223d4ddd35853d60f4c87ea2bcdc
Camilo Mason <camilomason@Camilos-MBP.home>
2019-09-18 16:05:32 -0400

    adding img anchors + small things (1.1)

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,2 +1,8 @@
 class Image < ApplicationRecord
+  def self.get_previous_image(current_image)
+    Image.where("images.id < ? ", current_image.id).order('created_at asc').last
+  end
+  def self.get_next_image(current_image)
+    Image.where("images.id > ? ", current_image.id).order('created_at asc').first
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'images#index' # main page
-
-  get '/:id', to: "images#show"
+  root 'images#index', as: "images" # main page
+  get '/:id', to: "images#show", as: "image" # not-so-main page
 end
