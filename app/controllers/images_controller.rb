a174013072c25f7e1600f1b545baa924a01ea086
Camilo Mason <camilomason@Camilos-MBP.home>
2019-09-18 22:16:39 -0400

    adding ssl certification
    plz hackers don't steal this :(

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -6,4 +6,8 @@ class ImagesController < ApplicationController
   def show
     @image = Image.find(params[:id])
   end
+
+  def ssl
+    render file: "#{Rails.root}/app/assets/files/E8DB7BA1796C667107D79492B04DF9FA.txt"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'images#index', as: "images" # main page
-  get '/:id', to: "images#show", as: "image" # not-so-main page
+  get '/images/:id', to: "images#show", as: "image" # not-so-main page
+
+  get '/.well-known/pki-validation/E8DB7BA1796C667107D79492B04DF9FA.txt', to: "images#ssl" # ssl validation
 end
