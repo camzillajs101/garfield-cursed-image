a50bdf8d38932773f551817baec5cc65214cc0b3
Camilo Mason <camilomason@Camilos-MBP.home>
2019-12-12 17:26:00 -0500

    adding secret env variable...
    spooky

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,51 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+let up, up2, down, down2, left, right, left2, right2, b, a, enter
+
+$(document).on('keydown',function(e){ // konami code
+  if (e.key === "ArrowUp"){
+    up = true;
+  }
+  if (up && e.key === "ArrowUp"){
+    up2 = true;
+  }
+  if (up && up2 && e.key === "ArrowDown"){
+    down = true;
+  }
+  if (up && up2 && down && e.key === "ArrowDown"){
+    down2 = true;
+  }
+  if (up && up2 && down && down2 && e.key === "ArrowLeft"){
+    left = true;
+  }
+  if (up && up2 && down && down2 && left && e.key === "ArrowRight"){
+    right = true;
+  }
+  if (up && up2 && down && down2 && left && right && e.key === "ArrowLeft"){
+    left2 = true;
+  }
+  if (up && up2 && down && down2 && left && right && left2 && e.key === "ArrowRight"){
+    right2 = true;
+  }
+  if (up && up2 && down && down2 && left && right && left2 && right2 && e.key === "b"){
+    b = true;
+  }
+  if (up && up2 && down && down2 && left && right && left2 && right2 && b && e.key === "a"){
+    a = true;
+  }
+  if (up && up2 && down && down2 && left && right && left2 && right2 && b && a && e.key === "Enter"){
+    alert("you did it!");
+    up = false;
+    up2 = false;
+    down = false;
+    down2 = false;
+    left = false;
+    right = false;
+    left2 = false;
+    right2 = false;
+    b = false;
+    a = false;
+  }
+})
